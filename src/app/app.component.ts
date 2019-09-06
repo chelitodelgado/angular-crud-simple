1e2ec058e6e1c0ed678cc3564923f7704e69de2d
Angel <cheloparedestorres@gmail.com>
2019-09-06 01:44:42 -0500

    UP ALL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
+ import { Employee} from './models/employee';
+ 
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-simple-crud';
+
+  employeeArray: Employee[] = [
+    { id: 1, name: "Angel", country: "MX"},
+    { id: 2, name: "Marisol", country: "MX" },
+    { id: 3, name: "Miguel", country: "MX" },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOrEdit() {
+    if (this.selectedEmployee.id == 0) {
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+
+    this.selectedEmployee = new Employee();
+
+  }
+
+  delete(){
+    if(confirm('Are you wnat to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
+
 }
